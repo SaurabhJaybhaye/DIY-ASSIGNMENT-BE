41bc566972bb41dbf2038afbc3b6839ece2bccc9
Saurabh Jaybhaye <saurabh.jaybhaye@opcito.com>
2023-09-17 22:26:58 +0530

    Added Read me file for using project BE

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -15,15 +15,4 @@ router.route("/:date").put(putMenu);
 
 //To get menus for the present week:
 
-// Request: GET /menu/filter?filterType=week
-// To get menus for the present month:
-
-// Request: GET /menu/filter?filterType=month
-// To get menus for the present year:
-
-// Request: GET /menu/filter?filterType=year
-// To get menus for a specific date (e.g., "2023-09-17"):
-
-// Request: GET /menu/filter?filterType=2023-09-17
-
 module.exports = router;
