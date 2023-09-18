82863beb31972a2a6380b8b2b5cc30b3535304a4
Saurabh Jaybhaye <saurabh.jaybhaye@opcito.com>
2023-09-18 10:56:38 +0530

    1. removed employee lunchCount and breakfastCount from database 2. added month and year filter to get employee details 3. Added lunchCount and breakfastCount virtually from model

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -9,18 +9,43 @@ const getAllEmployees = asyncHandler(async (req, resp) => {
   resp.json(allEmployees);
 });
 
-const getEmployee = asyncHandler(async (req, resp) => {
-  const empId = req.params.empId;
-  const employee = await Employee.find({ empId }).populate("meal");
-  resp.json(employee);
-});
+const getEmployee = asyncHandler(async (req, res) => {
+  try {
+    const { empId } = req.params;
+    const { month, year } = req.query;
+    const employee = await Employee.findOne({ empId }).populate("meal");
+    if (!employee) {
+      return res.status(404).json({ error: "Employee not found" });
+    }
+    if (month && year) {
+      employee.meal.lunch = employee.meal.lunch.filter(
+        (date) =>
+          new Date(date).getMonth() + 1 === parseInt(month) &&
+          new Date(date).getFullYear() === parseInt(year)
+      );
+      employee.meal.breakfast = employee.meal.breakfast.filter(
+        (date) =>
+          new Date(date).getMonth() + 1 === parseInt(month) &&
+          new Date(date).getFullYear() === parseInt(year)
+      );
+    } else if (year) {
+      employee.meal.lunch = employee.meal.lunch.filter(
+        (date) => new Date(date).getFullYear() === parseInt(year)
+      );
+      employee.meal.breakfast = employee.meal.breakfast.filter(
+        (date) => new Date(date).getFullYear() === parseInt(year)
+      );
+    }
 
-// put employees details
-// const putEmployee = asyncHandler(async (req, resp) => {
-//   const empId = req.params.empId;
-//   const employee = await Employee.find({ empId }).populate("meal");
-//   resp.json(employee);
-// });
+    employee.meal.lunchCount = employee.meal.lunch.length;
+    employee.meal.breakfastCount = employee.meal.breakfast.length;
+
+    res.json(employee);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 module.exports = {
   getAllEmployees,
